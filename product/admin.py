e8dca123a89a37ef5c05dc18b13acd5d615815b2
Rodrigue CODO <codorol22@gmail.com>
2022-07-21 02:48:17 +0500

    Create seperate urls files in each app

--- product/admin.py
+++ product/admin.py
@@ -1,9 +1,11 @@
+
 from django.contrib import admin
 from .models import Product
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
-    list_display:['id','name']
-    search_fields:['id','name']
+    
+    list_display = ["name","slug"]
+    search_fields = ["name","slug"]
 
 admin.site.register(Product,ProductAdmin)
